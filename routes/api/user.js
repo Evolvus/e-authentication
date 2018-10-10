bc0d7e28d02f192ec47e7030b8388b69a4e36c7f
KmKavya <kavyavshaiva@gmail.com>
2018-10-10 10:22:06 +0530

    removed AD authentication for FLUX_CDA

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,6 +4,7 @@ const debug = require("debug")("evolvus-e-authentication:routes:api:user");
 const _ = require("lodash");
 const user = require("@evolvus/evolvus-user");
 const application = require("@evolvus/evolvus-application");
+var applicationCode = process.env.APPLICATION_CODE || "SANDSTORM";
 
 const headerAttributes = ["tenantid", "entityid", "accessLevel"];
 const credentials = ["userName", "userPassword", "applicationCode"];
@@ -27,43 +28,62 @@ module.exports = (router) => {
       };
       try {
         let object = _.pick(req.body, credentials);
-        user.findUserName(object.userName, object.applicationCode)
-          .then((result) => {
-            if (result) {
-              let username = `${object.userName}${config.domain}`;
-              ad.authenticate(username, object.userPassword, function(err, auth) {
-                if (err) {
-                  debug('ERROR: ' + JSON.stringify(err));
-                  response.status = "401";
-                  response.description = `Active Directory Authentication failed!`;
-                  res.status(401).send(response);
-                } else if (auth) {
-                  debug('Both Console and Active Directory Authenticated!');
-                  response.description = `Both Console and Active Directory Authenticated!`;
-                  response.data = auth;
-                  res.status(200).send(result);
-                } else {
-                  debug('Active Directory Authentication failed!');
-                  response.status = "401";
-                  response.description = `Authentication failed!`;
-                  response.data = auth;
-                  res.status(401).send(response);
-                }
-              });
-            } else {
-              debug('Console Authentication failed!');
+        console.log("Application Code:",object.applicationCode);
+        if (object.applicationCode === applicationCode) {
+          user.findUserName(object.userName, object.applicationCode)
+            .then((result) => {
+              if (result) {
+                let username = `${object.userName}${config.domain}`;
+                ad.authenticate(username, object.userPassword, function (err, auth) {
+                  if (err) {
+                    debug('ERROR: ' + JSON.stringify(err));
+                    response.status = "401";
+                    response.description = `Active Directory Authentication failed!`;
+                    res.status(401).send(response);
+                  } else if (auth) {
+                    debug('Both Console and Active Directory Authenticated!');
+                    response.description = `Both Console and Active Directory Authenticated!`;
+                    response.data = auth;
+                    res.status(200).send(result);
+                  } else {
+                    debug('Active Directory Authentication failed!');
+                    response.status = "401";
+                    response.description = `Authentication failed!`;
+                    response.data = auth;
+                    res.status(401).send(response);
+                  }
+                });
+              } else {
+                debug('Console Authentication failed!');
+                response.status = "401";
+                response.description = `Console Authentication failed!`;
+                response.data = "";
+                res.status(401).send(response);
+              }
+            }).catch((e) => {
+              debug('Console Authentication failed!', e);
               response.status = "401";
               response.description = `Console Authentication failed!`;
-              response.data = "";
+              response.data = e;
               res.status(401).send(response);
-            }
+            });
+        } else {
+          debug('Received', credentials);
+          let body = _.pick(req.body, credentials);
+          debug('Login user', body.userName);
+          user.authenticate(body).then((user) => {
+            debug('User LoggedIn', user._id);
+            res.status(200).send(user);
           }).catch((e) => {
-            debug('Console Authentication failed!', e);
-            response.status = "401";
-            response.description = `Console Authentication failed!`;
-            response.data = e;
-            res.status(401).send(response);
+            debug('error', e);
+            const error = {
+              status: '401',
+              data: e,
+              description: `${e.toString()}`
+            };
+            res.status(401).send(error);
           });
+        }
       } catch (e) {
         debug("try-catch failed", e);
         response.status = 401;
